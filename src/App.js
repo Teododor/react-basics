be32bdfccbe6130da24c959e6a6cfa1e1373c91d
Axinte Teodor <teodoraxinte2002@gmail.com>
2024-02-04 17:47:18 +0200

    setState(when the page re-renders, where the variables point to, shallowMerging with setState, passing as objects vs passing as functions for making the process synchronous

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super(); // this is needed to call the constructor of the parent class
+
+    this.state = {
+      name:
+      {
+        firstName: 'TEODOR',
+        lastName: 'AXINTE'
+      },
+      company: 'UNI'
+    }
+
+  }
+
+  render() {
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            HI {this.state.name.firstName}, {this.state.name.lastName}! You are working at {this.state.company}
+          </p>
+
+          <button
+           
+            onClick={() => {
+              this.setState(() => {
+
+                return {
+                  name:
+                  {
+                    firstName: "IONUT",
+                    lastName: "TEODOR"
+                  },
+                  company: "MAI"
+                }
+              }, () => {console.log("CURRENT STATE IS: ", this.state)})
+            }}
+
+
+
+          >Change Name</button>
+        </header>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
